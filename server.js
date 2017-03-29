0ea6fd256013d56e4fae22c95cbfe89ab020f280
Dina Makled <dina.maklad@student.guc.edu.eg>
2017-03-29 19:38:14 +0200

    Basic Layout

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = require('./app/routes');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var DB_URI = "mongodb://localhost:27017/portfolio";
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect(DB_URI);
+app.use(router);
+
+app.listen(8080, function(){
+  console.log("Server is listening on port 8080");
+})
