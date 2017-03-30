e45fb406d4a66d5c3c6cb9874209ba46a3976466
Dina Makled <dina.maklad@student.guc.edu.eg>
2017-03-31 01:37:49 +0200

    Deal model added

--- app/models/Deal.js
+++ app/models/Deal.js
@@ -1,1 +1,47 @@
-// kjsdl
+var mongoose = require('mongoose');
+
+var dealSchema = mongoose.Schema({
+  title:{
+    type:String,
+    required:true
+  },
+  price:{
+    type:Number,
+    required:true
+  },
+  description:{
+    type:String,
+    required:true
+  },
+  start_date:{
+    type:{type: Date, default: Date.now},
+    required:true
+  },
+  end_date:{
+    type:{type: Date, default: Date.now},
+    required:true
+  },
+  terms:{
+    type:String
+  },
+  picture_path:{
+    type:String,
+    required:true
+  },
+  expired:{
+    type:Boolean,
+    required:true
+  },
+  client_id:{
+    type:String,
+    required:true
+  },
+  event_id:{
+    type:String,
+    required:true
+  }
+
+})
+
+var Deal = mongoose.model("deal", dealSchema);
+module.exports = Deal;
