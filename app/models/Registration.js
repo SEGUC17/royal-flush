9aae81bb7c0ca84124acd7f270e78bbd56d49448
ahmed mosaab <a7madmos3ab@gmail.com>
2017-04-04 10:33:44 +0200

    Add Registration schema

---
+++ app/models/Registration.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var registrationSchema = mongoose.Schema({
+    eventId: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true
+    }
+    tier: {
+        type: String,
+        required: true
+    },
+    acceptsRefundPolicy: {
+        type: Boolean,
+        required: true
+    }
+});
+
+var Registration = mongoose.model("registration", registrationSchema);
+module.exports = Registration;
