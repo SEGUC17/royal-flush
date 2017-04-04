6d3adf818096e4959440494fb7478f0aeaa5e8e6
ahmed mosaab <a7madmos3ab@gmail.com>
2017-04-04 10:55:11 +0200

    Create Registration controller

---
+++ app/controllers/registrationController.js
@@ -0,0 +1,22 @@
+var Registration = require('../models/Registration.js');
+
+var registrationController = {
+    addRegistration: function(req, res) {
+        var registration = new Registration(req.body);
+        registration.save(function(err, registration) {
+            if (err) {
+                res.json({
+                    error: err.message,
+                    data: null
+                });
+            } else {
+                res.json({
+                        error: null,
+                        data: registration
+                    }
+                });
+        });
+    }
+};
+
+module.exports = registrationController;
