b4031f84b4e44942de7623350df33e7cbc210b92
SeifIhab <saif.ihab96@outlook.com>
2017-04-05 20:17:23 +0200

    remove testing lines @Hazem_Ismail

--- app/controllers/administratorController.js
+++ app/controllers/administratorController.js
@@ -2,7 +2,7 @@ let Administrator = require('../models/Client');
 
 let administratorController = {
 
-  let administratorController = {
+//   let administratorController = {
 
   getAllUnverifiedClients:function(req, res){
 
@@ -14,15 +14,15 @@ let administratorController = {
               res.render('index', {clients});
       })
  }
-
-const clients = [{name:ahmed, verified:false, address:asd}, {name:omar, verified:true, address:qwe}, {name:hazem, verified:false, address:opo}];
-
-client.insertMany(clients,function(err, docs){
-  if(err)
-      res.send(err)
-})
-
 }
-}
+//const clients = [{name:ahmed, verified:false, address:asd}, {name:omar, verified:true, address:qwe}, {name:hazem, verified:false, address:opo}];
+
+// client.insertMany(clients,function(err, docs){
+//   if(err)
+//       res.send(err)
+// })
+
+// }
+// }
 
 module.exports = administratorController;
