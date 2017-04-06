8541e04044a879b116bbf883939c1a97581a2c41
mohamedelsharqawy <mohamed.elsharqawy@student.guc.edu.eg>
2017-04-06 15:25:33 +0300

    Update clientPassport.js

--- app/controllers/clientPassport.js
+++ app/controllers/clientPassport.js
@@ -19,6 +19,7 @@ module.exports = function(passport) {
     // LOCAL SIGNUP
 
     passport.use('local-signup', new LocalStrategy({
+//we use variables already deafined in local strategy but we set username as an email
 
         usernameField : 'email',
         passwordField : 'password',
@@ -28,21 +29,21 @@ module.exports = function(passport) {
 
         process.nextTick(function() {
 
-      //check if the user already exist
+      
         Client.findOne({ 'local.email' :  email }, function(err, client) {
-
+  //find client with the same entred email
             if (err)
-                return done(err);
-            if (client) {
+                return done(err);//if error we return error
+            if (client) {// if we find same email in the database
                 return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-            } else {
+            } else {//registred as a new client in the data base
 
                 var new_client = new Client();
 
                 new_client.local.email    = email;
                 new_client.local.password = new_client.generateHash(password);
                   // console.log(passport.session.email);
-                new_client.save(function(err) {
+                new_client.save(function(err) {//save it in the database
                     if (err)
                         throw err;
                     return done(null, new_client);
@@ -55,6 +56,7 @@ module.exports = function(passport) {
     // LOCAL LOGIN
 
     passport.use('local-login', new LocalStrategy({
+//we use variables already deafined in local strategy but we set username as an email
 
         usernameField : 'email',
         passwordField : 'password',
@@ -66,13 +68,13 @@ module.exports = function(passport) {
         Client.findOne({ 'local.email' :  email }, function(err, client) {
 
             if (err)
-                return done(err);
+                return done(err);//if error we return an error
 
-            if (!client)
+            if (!client)// we didn't find the email of the client in the database
                 return done(null, false, req.flash('loginMessage', 'You are not found'));
 
 
-            if (!Client.validPassword(password))
+            if (!Client.validPassword(password))// we find the client but wrong password
                 return done(null, false, req.flash('loginMessage', ' Wrong password'));
 
 
