1df4ac7f7ddc10835a84a74cdcdc15a64588f84f
seif igab <saif.ihab96outlook.com>
2017-04-06 20:51:40 +0200

    adding routes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var administratorController= require('./controllers/administratorController');
+var clientController=require('./controllers/clientController');
 var router = express.Router();
 
 //Add routes here
+router.get("/administratorLogin", administratorController.administratorLogin);
+router.get("/viewFeedback", clientController.viewFeedback);
+
 
 module.exports = router;
