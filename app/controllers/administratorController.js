4effe3ba2871a458d1ccbc3a0c50928f84bade1d
HazemIsmail0 <hazem.ismail@student.guc.edu.eg>
2017-04-06 13:45:55 +0200

    Update administratorController.js

--- app/controllers/administratorController.js
+++ app/controllers/administratorController.js
@@ -2,7 +2,6 @@ let Administrator = require('../models/Client');
 
 let administratorController = {
 
-//   let administratorController = {
 
   getAllUnverifiedClients:function(req, res){
 
@@ -15,14 +14,6 @@ let administratorController = {
       })
  }
 }
-//const clients = [{name:ahmed, verified:false, address:asd}, {name:omar, verified:true, address:qwe}, {name:hazem, verified:false, address:opo}];
 
-// client.insertMany(clients,function(err, docs){
-//   if(err)
-//       res.send(err)
-// })
-
-// }
-// }
 
 module.exports = administratorController;
