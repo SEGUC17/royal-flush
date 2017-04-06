b7e0fc00d0aa40c8520403e879dd45994d21e458
seif igab <saif.ihab96outlook.com>
2017-04-06 02:52:19 +0200

    doing the getAllClients and Search functions and routes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var clientController= require('./models/clientController');
+var userController=require('./models/userController');
 
 //Add routes here
+router.get('/collectingSubscription',clientController.collectingSubscription);
+router.get('/getAllClients',clientController.getAllClients);
+router.get('/searchForClients',clientController.searchForClients);
+router.get('/userSubscribeToClient',userController.userSubscribeToClient);
+router.get('/userUnsubscribe', userController.userUnsubscribe);
+
 
 module.exports = router;
