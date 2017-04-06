badb70f08238c740a0d7a9956c599549653319cf
a7madmos3ab <a7madmos3ab@gmail.com>
2017-04-06 20:15:15 +0200

    Update Client.js

--- app/models/Client.js
+++ app/models/Client.js
@@ -1,9 +1,57 @@
+// Added from Dina's Client model
+
 var mongoose = require('mongoose');
 
 var clientSchema= mongoose.Schema({
-  //Client schema
-
-
+  name:{
+    type:String,
+    required:true
+  },
+  username:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  email:{
+    type:String,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  address:{
+    type:String,
+    required:true
+  },
+  created:{
+    type:Date,
+    default:Date.now
+  },
+  phone:{
+    type:String,
+    required:true
+  },
+  verified:{
+    type:Boolean,
+    default:0
+  },
+  start_hour:{
+    type:String,
+    required:true
+  },
+  end_hour:{
+    type:String,
+    required:true
+  },
+  working_days:{
+    type:String,
+    required:true
+  },
+  category:{
+    type:String,
+    required:true
+  }
 })
 
 var Client = mongoose.model("client", clientSchema);
