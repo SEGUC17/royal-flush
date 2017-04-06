d8f8931c4176cd19f82ab3bb1ca41453db556d79
seif igab <saif.ihab96outlook.com>
2017-04-06 13:58:27 +0200

    updating and documentation

--- app/controllers/clientController.js
+++ app/controllers/clientController.js
@@ -4,17 +4,18 @@ let Client= require('../models/Client');
 let clientEvent= require('../models/clientEvent');
 
 let clientController ={
-  collectingSubscription:function(req,res){
+  collectingSubscription:function(req,res){// collecting ALL subscribed to clients and collect them for later usage
     let subscriptions;
-    Subscription.find({_id:req.params.client_id},function(err,subscriptions){
+    Subscription.find({userId:req.params.user_id},function(err,subscriptions){
       if(err){
         res.send(err.message);
       }else{
         subscriptions={subscriptions};
+        res.return(subscriptions);
       //  res.redirect('/notifyUsers',{subscriptions});
       }
     })
-    for (var i = 0; i < subscriptions.length; i++) {
+  /*  for (var i = 0; i < subscriptions.length; i++) {
       let notification = new Notification ({"clientId": subscriptions[i].clientId, "userId": subscriptions[i].userId, "viewed": false, "message": "new event created by "+req.session.name});
       notification.save(function(err, notification){
         if(err){
@@ -22,10 +23,10 @@ let clientController ={
         }else{
           res.redirect('/clientProfile');
         }
-      })
-    }
+      })*/
+    // }
 },
-  getAllClients:function(req, res){
+  getAllClients:function(req, res){ // ALL clients Not filtered for viewing porpose
     Client.find(function(err, clients){
       if(err){
         res.send(err.message);
@@ -34,11 +35,13 @@ let clientController ={
       }
     })
   },
-  searchForClients:function(req, res){
-    let es;
-    let cs;
+  searchForClients:function(req, res){// search function byy client name and event name
+    let es; // collection of events (temprory)
+    let cs;// collection of clients then will be have es added to it and passed
 
     let searchKey = req.body.searchKey ;
+
+    //search by name for all possible clients
     Client.find({ name : searchKey }, function(err, clients){
       if(err){
         res.send("error with search");
@@ -47,6 +50,8 @@ let clientController ={
         cs={clients};
       }
     });
+
+    // search by name for all possible Events
     clientEvent .find({eventName:searchKey} , function(err, clientEvents){
       if(err){
         res.send("error with search");
--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -6,10 +6,14 @@ let Notification= require('../models/Notification');
 let userController = {
   // Add user methods
   userSubscribeToClient:function(req,res){
+    // subscribe functions for the user
+
+    // make sure the client id provided is poresesnt
     Client.findOne({_id: req.body.clientId}, function(err, client){
       if(err){
         res.send('Client Error try again later');
       }else if (client){
+        //only if yes the subscription is created
         let subscription = new Subscription({"clientId": req.body.clientId,"userId": req.params.user_id});
         user.save(function(err, subscription){
           if(err){
@@ -26,6 +30,7 @@ let userController = {
 
   },
     userUnsubscribe:function(req,res){
+      //removal of subscription from memory 
       Subscription.remove({"clientId": req.body.userId,"userId": req.params.user_id});
       res.redirect('/userUnsubscribe');
     }
