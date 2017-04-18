e17fa844cdf29d4ab3e606772016caf970e74c64
amraliabugreedah <amrabugreedah@gmail.com>
2017-04-18 09:25:00 +0300

    Update edit-user-profile.service.ts

--- src/app/edit-user-profile.service.ts
+++ src/app/edit-user-profile.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers} from '@angular/http';
 import 'rxjs/Rx';
+import { Router } from '@angular/router';
 
 // const axios = require('axios');
 
 @Injectable()
 export class EditUserProfileService {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private router: Router) { }
 
   // update user profile
   editUserProfile(data: any){
       let bodyString = data;
       let headers      = new Headers({ 'Content-Type': 'application/json' });
        this.http.post('/api/editUserProfile', bodyString, { headers: headers }).toPromise();
+       this.router.navigate(['/userProfile']);
   }
 }
