eeecdd97379944a9201f30af3b7db69f79999151
Rawanelkady <rawanelkady@gmail.com>
2017-04-20 21:56:50 +0200

    Add files via upload

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import{RouterModule, Routes} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UnverifiedComponent } from './components/unverified/unverified.component';
+
+const appRoutes: Routes = [
+{path: 'unverified', component: UnverifiedComponent}
+
+]
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    UnverifiedComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
