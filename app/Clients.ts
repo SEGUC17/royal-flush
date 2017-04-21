47f69c82a22798c209ae29c5814e0c18df7fddb8
Rawanelkady <rawanelkady@gmail.com>
2017-04-21 19:25:48 +0200

    Add files via upload
    Front end

---
+++ app/Clients.ts
@@ -0,0 +1,14 @@
+export class Clients{
+    
+    
+    name      : String;
+         
+    verified: Boolean;
+
+    //I added that just to fix the suntax error
+    _id: String;
+    
+
+          //required:true
+       
+}
