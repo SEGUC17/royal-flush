003ec70b05060dd7912e794f508ef484283d5d22
Rawanelkady <rawanelkady@gmail.com>
2017-04-21 11:54:13 +0200

    Add files via upload

---
+++ models/Clients.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+//const config = require('./app/db');
+
+const clientSchema= mongoose.Schema({
+
+     local            : {
+          name      : {
+          type:String,
+          required :true
+        },
+          username  :{
+            type:String,
+            required:true,
+            unique:true
+          },
+          email     :{
+            type:String,
+            required:true
+
+          },
+          password  : {
+            type:String,
+            required:true
+          },
+          address   :{
+          type:  String,
+          required:true
+        },
+          created   :{
+            type : Date,
+            default : Date.now
+          },
+          phone     :{
+          type:  Number,
+          required:true
+        },
+          verified  :{
+          type:  Boolean,
+          //required:true
+        },
+          start_hour :{
+          type:  String,
+          required:true
+        },
+          end_hour  :{
+          type:  String,
+          required:true
+        },
+          working_days :{
+          type:  String,
+          required:true
+        },
+          category:{
+          type:String,
+          required: true
+}
+      }
+  });
+
+
+
+
+const client = module.exports = mongoose.model("client", clientSchema);
+//getting unverified clients
+module.exports.getClientByVerificationStatus = (callback) =>{
+  const query = {verified: "false"}
+  client.findByVerificationStatus(query, callback);
+}
+
+//module.exports = client;
